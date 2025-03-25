f9d59f17cbd094edebfb5afcd42c694bd48151f9
Rbeemu <reddy.beemu@gmail.com>
2025-03-25 14:52:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ session = conn.cursor()
 session.execute("USE DATABASE smoothies")
 session.execute("USE SCHEMA public")
 session.execute("SELECT FRUIT_NAME FROM fruit_options")
-my_dataframe = session.table('smoothies.public.fruit_options').select ((col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table('smoothies.public.fruit_options').select (col('FRUIT_NAME'), col('SEARCH_ON'))
 # my_dataframe = session.fetchall()
 
 pd_df = my_dataframe.to_pandas()
